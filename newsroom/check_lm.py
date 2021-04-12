162956da0cf795c540f708f391f93d716fa4781e
XinnuoXu <xuxinnuo@yahoo.com>
2021-04-12 07:44:43 +0100

    push newsroom dir

--- newsroom/check_lm.py
+++ newsroom/check_lm.py
@@ -10,8 +10,18 @@ INPUT_FILE = '/scratch/xxu/multi-multi/multi_multi_clean.jsonl'
 MAX_DOCS_IN_CLUSTER = 5
 MAX_LEN_SUMM = 200
 MAX_LEN_DOC = 500
-source_domain = 'cnn'
-target_domain = 'bbc'
+source_domain = 'nytimes'
+#target_domain = 'nytimes'
+#source_domain = 'washingtonpost'
+#target_domain = 'washingtonpost'
+#source_domain = 'theguardian'
+#source_domain = 'cnn'
+#target_domain = 'cnn'
+#target_domain = 'bbc'
+#source_domain = 'theguardian'
+target_domain = 'theguardian'
+source_domain = 'dailymail'
+#target_domain = 'dailymail'
 
 def cut_by_length(doc, max_length):
     sents = doc.split('\t')
@@ -44,8 +54,8 @@ def load_clusters():
                 summary = '\t'.join(pair['[TITLE]']) + '\t' + '\t'.join(pair['[SUMMARY]'])
             else:
                 summary = '\t'.join(pair['[SUMMARY]'])
-            document = cut_by_length(document, MAX_LEN_DOC)
-            summary = cut_by_length(summary, MAX_LEN_SUMM)
+            document = cut_by_length(document, MAX_LEN_DOC).replace('\t', ' ')
+            summary = cut_by_length(summary, MAX_LEN_SUMM).replace('\t', ' ')
             if source.find(source_domain) > -1:
                 source_domain_summary = summary
             if source.find(target_domain) > -1:
@@ -57,6 +67,47 @@ def load_clusters():
         elif target_domain_summary != '':
             res_target_single.append(target_domain_summary)
     return res_pairs, res_source_single, res_target_single
+
+def analysis_novelty(train_filename, test_filename, ngram, min_count=0):
+    from collections import Counter
+    def get_ngram(filename):
+        doc_grams = []
+        for line in open(filename):
+            document = line.strip().split()
+            for i in range(len(document)-ngram+1):
+                doc_grams.append(' '.join(document[i:i+ngram]))
+        return doc_grams
+    train_ngrams = Counter(get_ngram(train_filename))
+    train_ngrams = set([item for item in train_ngrams if train_ngrams[item]>=min_count])
+    test_ngrams = get_ngram(test_filename)
+    count = 0; novel = 0
+    for gram in test_ngrams:
+        if gram not in train_ngrams:
+            novel += 1
+        count += 1
+    return novel, count
+
+def ngram_novelty(train_filename, test_filename):
+    test_novel_num_uni = []; test_count_num_uni = []
+    test_novel_num_bi = []; test_count_num_bi = []
+    test_novel_num_tri = []; test_count_num_tri = []
+    test_novel_num_four = []; test_count_num_four = []
+    novel, count = analysis_novelty(train_filename, test_filename, 1)
+    test_novel_num_uni.append(novel)
+    test_count_num_uni.append(count)
+    novel, count = analysis_novelty(train_filename, test_filename, 2)
+    test_novel_num_bi.append(novel)
+    test_count_num_bi.append(count)
+    novel, count = analysis_novelty(train_filename, test_filename, 3)
+    test_novel_num_tri.append(novel)
+    test_count_num_tri.append(count)
+    novel, count = analysis_novelty(train_filename, test_filename, 4)
+    test_novel_num_four.append(novel)
+    test_count_num_four.append(count)
+    print ('Novelty uni-gram', sum(test_novel_num_uni)/sum(test_count_num_uni))
+    print ('Novelty bi-gram', sum(test_novel_num_bi)/sum(test_count_num_bi))
+    print ('Novelty tri-gram', sum(test_novel_num_tri)/sum(test_count_num_tri))
+    print ('Novelty four-gram', sum(test_novel_num_four)/sum(test_count_num_four))
 
 if __name__ == '__main__':
     res_pairs, res_source_single, res_target_single = load_clusters()
@@ -75,3 +126,15 @@ if __name__ == '__main__':
     fpout = open('check_lm_test_cross.txt', 'w')
     fpout.write('\n'.join([pair[1] for pair in res_pairs]))
     fpout.close()
+
+    # Same resource
+    train_filename = 'check_lm_train.txt'
+    test_filename = 'check_lm_test_same.txt'
+    print ("Same Resource:")
+    ngram_novelty(train_filename, test_filename)
+
+    # Different resource
+    train_filename = 'check_lm_train.txt'
+    test_filename = 'check_lm_test_cross.txt'
+    print ("Cross Resource:")
+    ngram_novelty(train_filename, test_filename)
--- newsroom/check_lm_gpt.sh
+++ newsroom/check_lm_gpt.sh
@@ -1,19 +1,23 @@
 export CUDA_VISIBLE_DEVICES=0
 
-EPOCH=30
+#EPOCH=30
+EPOCH=300
 LR=1e-5
 
-VAL_FILE=check_lm_test_same.txt
-#VAL_FILE=check_lm_test_cross.txt
+#VAL_FILE=check_lm_test_same.txt
+VAL_FILE=check_lm_test_cross.txt
+
+TRAIN_FILE=check_lm_train.txt
 
 python run_clm.py \
     --model_name_or_path gpt2 \
-    --train_file check_lm_train.txt \
+    --train_file ${TRAIN_FILE} \
     --validation_file ${VAL_FILE} \
     --per_gpu_train_batch_size 2 \
     --num_train_epochs ${EPOCH} \
     --learning_rate ${LR} \
-    --do_train \
     --do_eval \
     --overwrite_output_dir \
     --output_dir ./tmp/test-clm
+
+    #--do_train \
