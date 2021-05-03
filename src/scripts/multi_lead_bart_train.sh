0cd650ca0302401990d2d36f34c8ecaa33f6f321
not populated <xx6@cirrus-login1.ib0.icexa.epcc.ed.ac.uk>
2021-05-03 18:47:21 +0100

    add scripts

---
+++ src/scripts/multi_lead_bart_train.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+INPUT_DIR=../data/json_multi_lead_1000/multi
+OUTPUT_DIR=../saved_checkpoints/multi_lead_bart/
+MAX_SORCE_LEN=1024
+
+/bin/hostname -s
+python3 -m torch.distributed.launch \
+	--nproc_per_node=$NPROC_PER_NODE \
+	--nnodes=$SLURM_JOB_NUM_NODES \
+	--node_rank=$SLURM_PROCID \
+	--master_addr="$PARENT" --master_port="$MPORT" \
+	run_summarization.py \
+	--model_name_or_path facebook/bart-large \
+        --do_train \
+        --train_path ${INPUT_DIR} \
+        --text_column text \
+        --summary_column summary \
+        --output_dir ${OUTPUT_DIR} \
+	--max_source_length=${MAX_SORCE_LEN} \
+	--max_target_length=256 \
+	--num_train_epochs=15 \
+	--group_by_length=true \
+	--learning_rate=1e-04 \
+	--weight_decay=0.01 \
+	--max_grad_norm=0.1 \
+	--label_smoothing_factor=0.1 \
+	--lr_scheduler_type=polynomial \
+	--attention_dropout=0.1 \
+	--dropout=0.1 \
+	--warmup_steps=1000 \
+	--gradient_accumulation_steps=32 \
+        --per_device_train_batch_size=1 \
+        --per_device_eval_batch_size=1 \
+        --overwrite_output_dir \
+        --predict_with_generate \
+        --do_eval \
+        --validation_path ${INPUT_DIR} \
+	--evaluation_strategy=no \
+	--save_steps=3000 \
+	--logging_steps=50 \
+	--local_files_only true \
